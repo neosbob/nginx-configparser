9192be4899f6fd5c1dfb342d221c46c9afa48299
Po-Hsun Wu <neosbob@github.com>
2017-01-17 18:48:05 -0800

    Config_parser_test.cc changes; testing commit

--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,5 +1,8 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
+#include <iostream>
+using namespace std;
+
 
 TEST(NginxConfigParserTest, SimpleConfig) {
   NginxConfigParser parser;
@@ -9,3 +12,100 @@ TEST(NginxConfigParserTest, SimpleConfig) {
 
   EXPECT_TRUE(success);
 }
+
+class NginxConfigTest : public testing::Test {
+
+    protected:
+    bool Helper() {
+
+
+    }
+
+    NginxConfigParser parse;
+    NginxConfig out_config_;
+
+
+};
+
+TEST_F(NginxConfigTest, ToString) {
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("example_config", &out_config);
+
+    std::string check = out_config.ToString();
+
+    cout << check;
+
+    ASSERT_EQ( check , check);
+
+}
+
+class NginxConfigParserTest : public testing::Test {
+
+    protected:
+
+    bool Helper() {
+
+
+    }
+
+    NginxConfigParser parse;
+    NginxConfig out_config_;
+
+
+
+};
+
+TEST_F(NginxConfigParserTest, TokenTypeAsString) {
+    
+
+}
+
+TEST_F(NginxConfigParserTest, ParseToken) {
+    
+
+}
+
+TEST_F(NginxConfigParserTest, Parse) {
+    
+
+}
+
+class NginxConfigStatementTest : public testing::Test {
+
+    protected:
+
+    bool Helper() {
+
+
+    }
+
+    NginxConfigParser parse;
+    NginxConfig out_config_;
+
+
+
+};
+
+TEST_F(NginxConfigStatementTest, ToString2) {
+    
+
+}
+
+/*
+int main(int argc, char **argv) {
+  //::testing::InitGoogleTest(&argc, argv);
+  //return RUN_ALL_TESTS();
+  
+    NginxConfigParser parser;
+    NginxConfig out_config;
+
+    bool success = parser.Parse("example_config", &out_config);
+
+    std::string check = out_config.ToString(5);
+
+    cout << check;
+
+    //EXPECT_STREQ( check , check);
+}*/
